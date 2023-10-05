617531399ee85dc06789059fe775fa6c858ed401
Aidar <aydararzybekov2002@mail.ru>
2023-10-05 17:17:32 +0600

    update graph

--- src/components/Graphic.tsx
+++ src/components/Graphic.tsx
@@ -24,20 +24,20 @@ interface CustomLabelProps {
 }
 
 const data: DataItem[] = [
-	{ name: 'Янв', total: 200 },
-	{ name: 'Фев', total: 150 },
-	{ name: 'Март', total: 300 },
-	{ name: 'Апр', total: 180 },
-	{ name: 'Май', total: 250 },
+	{ name: 'Янв', total: 560 },
+	{ name: 'Фев', total: 590 },
+	{ name: 'Март', total: 1200 },
+	{ name: 'Апр', total: 1800 },
+	{ name: 'Май', total: 2500 },
 ]
 
 export const Graphic: React.FC = () => {
 	const [activeBarIndex, setActiveBarIndex] = useState<number | null>(null)
-	const [activeValue, setActiveValue] = useState<number | null>(null)
+	const [activeValue, setActiveValue] = useState<number | null>(300)
 
 	const renderCustomLabel = (props: CustomLabelProps) => {
-		const { x, y, width, value } = props
-		const barWidth = width || 0
+		const { x, y, value } = props
+
 		if (
 			x !== undefined &&
 			y !== undefined &&
@@ -45,22 +45,18 @@ export const Graphic: React.FC = () => {
 			value === activeValue
 		) {
 			return (
-				<text
-					x={x + barWidth / 2}
-					y={y - 10}
-					textAnchor='middle'
-					fill='#fff'
-					fontSize={14}
-				>
-					{`$${value}`}
-				</text>
+				<foreignObject x={x - 20} y={y - 28} width='55px' height='30px'>
+					<div className='bg-[#65FF8E] flex items-center justify-center text-black rounded-md z-30 px-2 min-w-[40px] min-h-[20px]'>
+						{value}
+					</div>
+				</foreignObject>
 			)
 		}
 		return null
 	}
 
 	return (
-		<ResponsiveContainer width='100%' height={350}>
+		<ResponsiveContainer width='20%' height={350}>
 			<BarChart data={data}>
 				<XAxis
 					dataKey='name'
@@ -68,24 +64,30 @@ export const Graphic: React.FC = () => {
 					fontSize={12}
 					tickLine={false}
 					axisLine={false}
+					textAnchor='middle'
 				/>
 				<YAxis
 					stroke='#888888'
 					fontSize={12}
 					tickLine={false}
 					axisLine={false}
-					tickFormatter={(value: number) => `$${value}`}
+					tickFormatter={(value: number) => `${value}`}
 				/>
-				<Bar dataKey='total' radius={[4, 4, 0, 0]}>
+				<Bar dataKey='total' radius={[4, 4, 0, 0]} width={16}>
 					<LabelList
+						width={16}
 						dataKey='total'
 						//@ts-expect-error
 						content={renderCustomLabel}
 					/>
 					{data.map((entry, index) => (
 						<Cell
+							width={16}
 							key={index}
-							fill={activeBarIndex === index ? 'red' : '#000AFF'}
+							fill={'#000AFF'}
+							className={`transition duration-300 ease-in-out transform hover:shadow-lg ${
+								activeBarIndex === index ? 'active' : ''
+							}`}
 							onMouseEnter={() => {
 								setActiveBarIndex(index)
 								setActiveValue(entry.total)
